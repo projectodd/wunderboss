c1b96d57e16b4450bc37d3ca263f14f35b7a5e7b
Toby Crawley <toby@tcrawley.org>
2013-11-20 18:53:13 -0500

    Rename some things.

--- modules/clojure/src/main/clojure/wunderboss/ring.clj
+++ modules/clojure/src/main/clojure/wunderboss/ring.clj
@@ -44,46 +44,46 @@
         (.addAll to-headers k v)
         (.add to-headers k v)))))
 
-(defprotocol BodyCoercion
-  (coerce-body [body exchange]))
+(defprotocol BodyWriter
+  (write-body [body exchange]))
 
-(extend-protocol BodyCoercion
+(extend-protocol BodyWriter
 
   Object
-  (coerce-body [body _]
+  (write-body [body _]
     (throw (IllegalStateException. (str "Can't coerce body of type " (class body)))))
 
   nil
-  (coerce-body [_ _]
+  (write-body [_ _]
     (throw (IllegalStateException. "Can't coerce nil body")))
 
   String
-  (coerce-body [body ^OutputStream os]
+  (write-body [body ^OutputStream os]
     (.write os (.getBytes body)))
 
   ISeq
-  (coerce-body [body ^OutputStream os]
+  (write-body [body ^OutputStream os]
     (doseq [fragment body]
-      (coerce-body fragment os)))
+      (write-body fragment os)))
 
   File
-  (coerce-body [body ^OutputStream os]
+  (write-body [body ^OutputStream os]
     (io/copy body os))
 
   InputStream
-  (coerce-body [body ^OutputStream os]
+  (write-body [body ^OutputStream os]
     (with-open [body body]
       (io/copy body os))))
 
-(defn- populate-response [^HttpServerExchange exchange {:keys [status headers body]}]
+(defn- write-response [^HttpServerExchange exchange {:keys [status headers body]}]
   (when status
     (.setResponseCode exchange status))
   (merge-headers (.getResponseHeaders exchange) headers)
-  (coerce-body body (.getOutputStream exchange)))
+  (write-body body (.getOutputStream exchange)))
 
 (defn handle-request [f ^HttpServerExchange exchange]
   (.startBlocking exchange)
   (with-open [output-stream (.getOutputStream exchange)]
     (if-let [response (f (ring-request exchange))]
-      (populate-response exchange response)
+      (write-response exchange response)
       (throw (NullPointerException. "Ring handler returned nil")))))
