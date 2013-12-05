99c008fbe509ab0112a830447809b1effa484bbc
Ben Browning <bbrownin@redhat.com>
2013-12-05 15:19:52 -0500

    Avoid looking up a couple of Ruby methods on every Rack request

--- modules/ruby/src/main/java/org/projectodd/wunderboss/ruby/rack/RackHandler.java
+++ modules/ruby/src/main/java/org/projectodd/wunderboss/ruby/rack/RackHandler.java
@@ -6,7 +6,7 @@ import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.RubyHash;
 import org.jruby.RubyModule;
-import org.jruby.runtime.Helpers;
+import org.jruby.internal.runtime.methods.DynamicMethod;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.projectodd.wunderboss.ruby.RubyHelper;
@@ -15,7 +15,7 @@ import java.io.IOException;
 
 public class RackHandler implements HttpHandler {
 
-    public RackHandler(IRubyObject rackApplication, String context) throws IOException {
+    public RackHandler(final IRubyObject rackApplication, final String context) throws IOException {
         this.rackApplication = rackApplication;
         this.context = context;
         runtime = rackApplication.getRuntime();
@@ -24,10 +24,14 @@ public class RackHandler implements HttpHandler {
         rackChannelClass = RackChannel.createRackChannelClass(runtime);
         rackResponderClass = RackResponder.createRackResponderClass(runtime);
         rackEnvironment = new RackEnvironment(runtime);
+
+        // Lookup these methods once, instead of on every request
+        rackCallMethod = rackApplication.getMetaClass().searchMethod(RACK_CALL_METHOD_NAME);
+        responseHandleMethod = responseModule.getMetaClass().searchMethod(RESPONSE_HANDLER_METHOD_NAME);
     }
 
     @Override
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
+    public void handleRequest(final HttpServerExchange exchange) throws Exception {
         if (exchange.isInIoThread()) {
             exchange.dispatch(this);
             return;
@@ -39,9 +43,11 @@ public class RackHandler implements HttpHandler {
             inputChannel = new RackChannel(runtime, rackChannelClass, exchange.getInputStream());
             RubyHash rackEnvHash = rackEnvironment.getEnv(exchange, inputChannel, context);
             ThreadContext context = runtime.getCurrentContext();
-            IRubyObject rackResponse = rackApplication.callMethod(context, "call", rackEnvHash);
+            IRubyObject rackResponse = rackCallMethod.call(context, rackApplication,
+                    rackApplication.getMetaClass(), RACK_CALL_METHOD_NAME, rackEnvHash);
             RackResponder rackResponder = new RackResponder(runtime, rackResponderClass, exchange);
-            Helpers.invoke(context, responseModule, RESPONSE_HANDLER_METHOD_NAME, rackResponse, rackResponder);
+            responseHandleMethod.call(context, responseModule, responseModule.getMetaClass(),
+                    RESPONSE_HANDLER_METHOD_NAME, rackResponse, rackResponder);
         } finally {
             if (inputChannel != null) {
                 inputChannel.close();
@@ -57,7 +63,10 @@ public class RackHandler implements HttpHandler {
     private RubyClass rackChannelClass;
     private RubyClass rackResponderClass;
     private RackEnvironment rackEnvironment;
+    private DynamicMethod rackCallMethod;
+    private DynamicMethod responseHandleMethod;
 
+    public static final String RACK_CALL_METHOD_NAME = "call";
     public static final String RESPONSE_HANDLER_RB = "wunderboss/rack/response_handler";
     public static final String RESPONSE_HANDLER_CLASS_NAME = "WunderBoss::Rack::ResponseHandler";
     public static final String RESPONSE_HANDLER_METHOD_NAME = "handle";
