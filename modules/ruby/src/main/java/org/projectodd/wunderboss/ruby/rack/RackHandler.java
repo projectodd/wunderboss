7e51cba42dd720d01c01c00203a4d01bebe340d8
Ben Browning <bbrownin@redhat.com>
2014-01-06 08:52:42 -0500

    Don't cache the Rack call method since it causes issues under Rails 3.2

--- modules/ruby/src/main/java/org/projectodd/wunderboss/ruby/rack/RackHandler.java
+++ modules/ruby/src/main/java/org/projectodd/wunderboss/ruby/rack/RackHandler.java
@@ -6,7 +6,7 @@ import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.RubyHash;
 import org.jruby.RubyModule;
-import org.jruby.internal.runtime.methods.DynamicMethod;
+import org.jruby.runtime.Helpers;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.projectodd.wunderboss.ruby.RubyHelper;
@@ -24,10 +24,6 @@ public class RackHandler implements HttpHandler {
         rackChannelClass = RackChannel.createRackChannelClass(runtime);
         rackResponderClass = RackResponder.createRackResponderClass(runtime);
         rackEnvironment = new RackEnvironment(runtime);
-
-        // Lookup these methods once, instead of on every request
-        rackCallMethod = rackApplication.getMetaClass().searchMethod(RACK_CALL_METHOD_NAME);
-        responseHandleMethod = responseModule.getMetaClass().searchMethod(RESPONSE_HANDLER_METHOD_NAME);
     }
 
     @Override
@@ -43,11 +39,9 @@ public class RackHandler implements HttpHandler {
             inputChannel = new RackChannel(runtime, rackChannelClass, exchange.getInputStream());
             RubyHash rackEnvHash = rackEnvironment.getEnv(exchange, inputChannel, context);
             ThreadContext context = runtime.getCurrentContext();
-            IRubyObject rackResponse = rackCallMethod.call(context, rackApplication,
-                    rackApplication.getMetaClass(), RACK_CALL_METHOD_NAME, rackEnvHash);
+            IRubyObject rackResponse = rackApplication.callMethod(context, "call", rackEnvHash);
             RackResponder rackResponder = new RackResponder(runtime, rackResponderClass, exchange);
-            responseHandleMethod.call(context, responseModule, responseModule.getMetaClass(),
-                    RESPONSE_HANDLER_METHOD_NAME, rackResponse, rackResponder);
+            Helpers.invoke(context, responseModule, RESPONSE_HANDLER_METHOD_NAME, rackResponse, rackResponder);
         } finally {
             if (inputChannel != null) {
                 inputChannel.close();
@@ -63,10 +57,7 @@ public class RackHandler implements HttpHandler {
     private RubyClass rackChannelClass;
     private RubyClass rackResponderClass;
     private RackEnvironment rackEnvironment;
-    private DynamicMethod rackCallMethod;
-    private DynamicMethod responseHandleMethod;
 
-    public static final String RACK_CALL_METHOD_NAME = "call";
     public static final String RESPONSE_HANDLER_RB = "wunderboss/rack/response_handler";
     public static final String RESPONSE_HANDLER_CLASS_NAME = "WunderBoss::Rack::ResponseHandler";
     public static final String RESPONSE_HANDLER_METHOD_NAME = "handle";
