6ce3919987724d48a99555d38b2035d2b7e28028
Ben Browning <bbrownin@redhat.com>
2014-01-06 14:44:19 -0500

    WunderBoss.container should always return a new container from Ruby
    The previous behavior of sometimes returning a new container or
    sometimes reusing an existing container depending on the arguments
    passed in was confusing and took me a while to track down why it was
    causing some TorqBox spec failures.

--- modules/ruby/src/main/ruby/wunderboss.rb
+++ modules/ruby/src/main/ruby/wunderboss.rb
@@ -1,13 +1,11 @@
 module WunderBoss
   class << self
-    attr_reader :container
         
     def container(opts = nil)
       if opts
         Java::OrgProjectoddWunderboss::WunderBoss.new(opts)
       else
-        @container = Java::OrgProjectoddWunderboss::WunderBoss.new unless @container
-        @container
+        Java::OrgProjectoddWunderboss::WunderBoss.new
       end
     end
   end
