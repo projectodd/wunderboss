85302dce251cd05af18ec07e0bb5b393726023d6
Ben Browning <bbrownin@redhat.com>
2014-02-28 13:17:42 -0500

    We don't need to do anything w/ bundler, actually

--- modules/wildfly/src/main/java/org/projectodd/wunderboss/wildfly/WildFlyRubyLanguage.java
+++ modules/wildfly/src/main/java/org/projectodd/wunderboss/wildfly/WildFlyRubyLanguage.java
@@ -47,8 +47,6 @@ public class WildFlyRubyLanguage extends RubyLanguage {
             }
         }
         swizzleResourceLoaders(loaderSpecs);
-
-        // eval("require 'bundler/setup'");
     }
 
     @Override
