0c3326bcf98afb4899274f196e95efee3cbd9f36
Ben Browning <bbrownin@redhat.com>
2014-04-25 20:38:37 -0400

    Forgot license headers on WildFlyMessaging classes

--- modules/wildfly/src/main/java/org/projectodd/wunderboss/wildfly/WildFlyMessaging.java
+++ modules/wildfly/src/main/java/org/projectodd/wunderboss/wildfly/WildFlyMessaging.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.projectodd.wunderboss.wildfly;
 
 import org.hornetq.jms.server.JMSServerManager;
@@ -5,7 +21,6 @@ import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.ServiceRegistry;
 import org.projectodd.wunderboss.Options;
 import org.projectodd.wunderboss.WunderBoss;
-import org.projectodd.wunderboss.messaging.Endpoint;
 import org.projectodd.wunderboss.messaging.hornetq.HornetQEndpoint;
 import org.projectodd.wunderboss.messaging.hornetq.HornetQMessaging;
 
