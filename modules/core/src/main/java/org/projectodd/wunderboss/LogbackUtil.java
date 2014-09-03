61c1582ab9230b53de51cedf914651bcbd19027d
Toby Crawley <toby@tcrawley.org>
2014-09-03 09:49:13 -0400

    Simplify LogBack configuration.

--- modules/core/src/main/java/org/projectodd/wunderboss/LogbackUtil.java
+++ modules/core/src/main/java/org/projectodd/wunderboss/LogbackUtil.java
@@ -18,9 +18,11 @@ package org.projectodd.wunderboss;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.joran.spi.JoranException;
 import org.slf4j.Logger;
+
+import java.net.URL;
 
 public class LogbackUtil {
 
@@ -30,23 +32,17 @@ public class LogbackUtil {
     }
 
     public static void configureLogback(Object rawContext) {
-        if (!(rawContext instanceof LoggerContext)) {
-            return;
-        }
-
-        LoggerContext context = (LoggerContext)rawContext;
-        ClassLoader cl = LogbackUtil.class.getClassLoader();
-        //TODO: see if there is a better way to determine if logback will self-configure
-        if (System.getProperty("logback.configurationFile") == null &&
-                cl.getResource("logback.xml") == null &&
-                cl.getResource("logback.groovy") == null) {
-            context.reset();
-            JoranConfigurator configurator = new JoranConfigurator();
-            configurator.setContext(context);
-            try {
-                configurator.doConfigure(cl.getResource("logback-default.xml"));
-            } catch (JoranException e) {
-                throw new RuntimeException("Failed to load default logging configuration", e);
+        if (rawContext instanceof LoggerContext) {
+            LoggerContext context = (LoggerContext)rawContext;
+            ContextInitializer initializer = new ContextInitializer(context);
+            if (initializer.findURLOfDefaultConfigurationFile(false) == null) {
+                context.reset();
+                URL defaultConfig = context.getClass().getClassLoader().getResource("logback-default.xml");
+                try {
+                    initializer.configureByResource(defaultConfig);
+                } catch (JoranException e) {
+                    throw new RuntimeException("Failed to load default logging configuration", e);
+                }
             }
         }
     }
