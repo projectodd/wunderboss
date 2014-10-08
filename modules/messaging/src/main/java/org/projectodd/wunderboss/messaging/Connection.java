410aa5a66965ab14903b62d357a383146879fd97
Jim Crossley <jim@crossleys.org>
2014-10-07 20:06:20 -0400

    XA identifier needs to be public for clients

--- modules/messaging/src/main/java/org/projectodd/wunderboss/messaging/Connection.java
+++ modules/messaging/src/main/java/org/projectodd/wunderboss/messaging/Connection.java
@@ -22,7 +22,7 @@ import java.util.Map;
 
 public interface Connection extends AutoCloseable {
 
-    static final Object XA = new Object();
+    public static final Object XA = new Object();
 
     class CreateSessionOption extends Option {
         public static final CreateSessionOption MODE = opt("mode", Session.Mode.AUTO_ACK, CreateSessionOption.class);
