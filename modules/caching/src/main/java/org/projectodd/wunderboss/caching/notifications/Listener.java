2b0bc3ac78661bffe48f5ef47c41edcddfb3b727
Jim Crossley <jim@crossleys.org>
2014-12-02 11:21:20 -0500

    Listener factory method

--- modules/caching/src/main/java/org/projectodd/wunderboss/caching/notifications/Listener.java
+++ modules/caching/src/main/java/org/projectodd/wunderboss/caching/notifications/Listener.java
@@ -37,6 +37,39 @@ public class Listener {
         this.handler = handler;
     }
 
+    public static Listener listen(Handler handler, String type) {
+        switch (type) {
+        case "cache_entries_evicted":
+            return new CacheEntriesEvicted(handler);
+        case "cache_entry_activated":
+            return new CacheEntryActivated(handler);
+        case "cache_entry_created":
+            return new CacheEntryCreated(handler);
+        case "cache_entry_invalidated":
+            return new CacheEntryInvalidated(handler);
+        case "cache_entry_loaded":
+            return new CacheEntryLoaded(handler);
+        case "cache_entry_modified":
+            return new CacheEntryModified(handler);
+        case "cache_entry_passivated":
+            return new CacheEntryPassivated(handler);
+        case "cache_entry_removed":
+            return new CacheEntryRemoved(handler);
+        case "cache_entry_visited":
+            return new CacheEntryVisited(handler);
+        case "data_rehashed":
+            return new DataRehashed(handler);
+        case "topology_changed":
+            return new TopologyChanged(handler);
+        case "transaction_completed":
+            return new TransactionCompleted(handler);
+        case "transaction_registered":
+            return new TransactionRegistered(handler);
+        default:
+            throw new IllegalArgumentException("Invalid listener type");
+        }
+    }
+
     @org.infinispan.notifications.Listener
     public static class CacheEntriesEvicted extends Listener {
         public CacheEntriesEvicted(Handler handler) {
