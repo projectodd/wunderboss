3dc689728a81176290467c62e9bc8413b0aa9ab3
Toby Crawley <toby@tcrawley.org>
2015-07-24 17:01:17 -0400

    Move ci build to script.

---
+++ support/bees-ci-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This is the bees CI build. Any changes to the build script should be
+# here instead if in the bees config.
+
+set -e
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+function mark {
+    echo
+    echo "=============================================="
+    echo $1
+    date
+    echo "=============================================="
+    echo
+}
+
+java -version
+mvn -version
+git clean -fd
+
+mark "Cleaning"
+mvn -B clean
+
+mark "Reversioning"
+mvn -B versions:set -DnewVersion=1.x.incremental.${BUILD_NUMBER}
+
+mark "Building"
+mvn -B -s ${SETTINGS_FILE} -Pbees install deploy
