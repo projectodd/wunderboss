bfe5f39a01631888dbb1115bd33f9d0f248024ad
Jim Crossley <jim@crossleys.org>
2015-08-01 09:48:19 -0400

    Bump ispan to ver 7 to fix [IMMUTANT-454]
    Unfortunately, this won't work in 8.2, since the interface for Cache
    changed in 7. We'll address that in the next commit.

--- modules/caching/src/main/java/org/projectodd/wunderboss/caching/CacheWithCodec.java
+++ modules/caching/src/main/java/org/projectodd/wunderboss/caching/CacheWithCodec.java
@@ -18,9 +18,12 @@ package org.projectodd.wunderboss.caching;
 
 import org.infinispan.Cache;
 import org.infinispan.AbstractDelegatingCache;
-import org.infinispan.util.concurrent.NotifyingFuture;
+import org.infinispan.commons.util.CloseableIterator;
+import org.infinispan.commons.util.CloseableIteratorCollection;
+import org.infinispan.commons.util.CloseableIteratorSet;
 import org.projectodd.wunderboss.codecs.Codec;
 import java.util.concurrent.TimeUnit;
+import java.util.AbstractCollection;
 import java.util.Collection;
 import java.util.Map;
 import java.util.AbstractMap.SimpleImmutableEntry;
@@ -164,35 +167,73 @@ public class CacheWithCodec extends AbstractDelegatingCache {
     }
 
     @Override
-    public Set keySet() {
-        Set keys = super.keySet();
-        Set result = new HashSet(keys.size());
-        for (Object k: keys) {
-            result.add(decode(k));
-        }
-        return result;
+    public CloseableIteratorSet keySet() {
+        return new EncodedSet(super.keySet());
     }
 
     @Override
-    public Set entrySet() {
-        Set entries = super.entrySet();
-        Set result = new HashSet(entries.size());
-        for (Object o: entries) {
-            Entry entry = (Entry) o;
-            result.add(new SimpleImmutableEntry(decode(entry.getKey()), decode(entry.getValue())));
-        }
-        return result;
+    public CloseableIteratorSet entrySet() {
+        return new EncodedEntrySet(super.entrySet());
     }
 
     @Override
-    public Collection values() {
-        Collection values = super.values();
-        Collection result = new ArrayList(values.size());
-        for (Object v: values) {
-            result.add(decode(v));
-        }
-        return result;
+    public CloseableIteratorCollection values() {
+        return new EncodedCollection(super.values());
     }
 
     private Codec codec;
+
+    class Decoded implements CloseableIterator {
+        Decoded(CloseableIterator iterator) {
+            this.iterator = iterator;
+        }
+        public void close() {
+            iterator.close();
+        }
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+        public Object next() {
+            return decode(iterator.next());
+        }
+        protected CloseableIterator iterator;
+    }
+
+    class DecodedEntry extends Decoded {
+        DecodedEntry(CloseableIterator iterator) {
+            super(iterator);
+        }
+        public Object next() {
+            Entry e = (Entry) iterator.next();
+            return new SimpleImmutableEntry(decode(e.getKey()), decode(e.getValue()));
+        }
+    }
+
+    class EncodedCollection extends AbstractCollection implements CloseableIteratorCollection {
+        EncodedCollection(CloseableIteratorCollection collection) {
+            this.collection = collection;
+        }
+        public int size() {
+            return CacheWithCodec.this.size();
+        }
+        public CloseableIterator iterator() {
+            return new Decoded(collection.iterator());
+        }
+        protected CloseableIteratorCollection collection;
+    }
+
+    class EncodedSet extends EncodedCollection implements CloseableIteratorSet {
+        EncodedSet(CloseableIteratorCollection collection) {
+            super(collection);
+        }
+    }
+
+    class EncodedEntrySet extends EncodedSet {
+        EncodedEntrySet(CloseableIteratorCollection collection) {
+            super(collection);
+        }
+        public CloseableIterator iterator() {
+            return new DecodedEntry(collection.iterator());
+        }
+    }
 }
