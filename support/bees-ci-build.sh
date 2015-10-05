d69a6ed2b92edd6fede3bc3e9f6e5ae3cd271ef9
Toby Crawley <toby@tcrawley.org>
2015-10-05 16:31:12 -0400

    Add ci build script for 0.8.x.

---
+++ support/bees-ci-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This is the bees CI build. Any changes to the build script should be
+# here instead if in the bees config.
+
+set -e
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+function mark {
+    echo
+    echo "=============================================="
+    echo $1
+    date
+    echo "=============================================="
+    echo
+}
+
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
+
+mark "Starting build script"
+java -version
+mvn -version
+git clean -fd
+
+mark "Cleaning"
+mvn -B clean
+
+mark "Reversioning"
+mvn -B versions:set -DnewVersion=0.8.x.incremental.${BUILD_NUMBER}
+
+mark "Building"
+mvn -B -s ${SETTINGS_FILE} install
+
+mark "Deploying"
+mvn -B -s ${SETTINGS_FILE} -Pbees deploy
